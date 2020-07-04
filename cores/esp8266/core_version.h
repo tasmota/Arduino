1ffba99eb833c4b4f67b6da4dcc4abdd0c95742c
Jason2866 <24528715+Jason2866@users.noreply.github.com>
2020-07-04 18:30:57 +0200

    Tasmota Core 2.8

--- cores/esp8266/core_version.h
+++ cores/esp8266/core_version.h
@@ -1,12 +1,4 @@
-#ifndef ARDUINO_ESP8266_GIT_VER
-#define ARDUINO_ESP8266_GIT_VER 0x00000000
-#endif
-
-#ifndef ARDUINO_ESP8266_GIT_DESC
-#define ARDUINO_ESP8266_GIT_DESC unspecified
-#endif
-
-// ARDUINO_ESP8266_RELEASE is defined for released versions as a string containing the version name, i.e. "2_3_0_RC1"
-// ARDUINO_ESP8266_RELEASE is used in the core internally. Please use ESP.getCoreVersion() function instead.
-
-// ARDUINO_ESP8266_RELEASE_<version number> are defined for releases, for use in #ifdef... constructs
+#define ARDUINO_ESP8266_GIT_VER 0xed25dc6
+#define ARDUINO_ESP8266_GIT_DESC 2.8.0
+#define ARDUINO_ESP8266_RELEASE_2_8_0
+#define ARDUINO_ESP8266_RELEASE "2_8_0"
