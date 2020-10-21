4f94abc1547bc7977e54fdb39e85250c7ba4c94f
Jason2866 <24528715+Jason2866@users.noreply.github.com>
2020-10-21 18:11:31 +0200

    Update cencode.cpp

--- cores/esp8266/libb64/cencode.cpp
+++ cores/esp8266/libb64/cencode.cpp
@@ -13,7 +13,7 @@ void base64_init_encodestate(base64_encodestate* state_in){
   state_in->step = step_A;
   state_in->result = 0;
   state_in->stepcount = 0;
-  state_in->stepsnewline = -1;
+  state_in->stepsnewline = BASE64_CHARS_PER_LINE;
 }
 
 
@@ -117,7 +117,7 @@ int base64_encode_blockend(char* code_out, base64_encodestate* state_in){
 
 int base64_encode_chars(const char* plaintext_in, int length_in, char* code_out){
   base64_encodestate _state;
-  base64_init_encodestate(&_state);
+  base64_init_encodestate_nonewlines(&_state);
   int len = base64_encode_block(plaintext_in, length_in, code_out, &_state);
   return len + base64_encode_blockend((code_out + len), &_state);
 }
